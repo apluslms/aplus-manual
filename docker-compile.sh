eca7301f19bcf3c115ff3b41fe3447a685653695
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2018-01-22 03:00:54 +0200

    Use current user in container, so we don't create files as root

--- docker-compile.sh
+++ docker-compile.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker run --rm -v $(pwd):/compile apluslms/compile-rst make touchrst
-docker run --rm -v $(pwd):/compile -e "STATIC_CONTENT_HOST=http://localhost:8080/static/default" apluslms/compile-rst make html
+
+docker run --rm \
+  -v $(pwd):/compile \
+  -u $(id -u):$(id -g) \
+  -e "STATIC_CONTENT_HOST=http://localhost:8080/static/default" \
+  apluslms/compile-rst \
+  make touchrst html
