21e133b6654815747c9886c93d60bd35d80fba2b
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2018-12-05 13:21:23 +0200

    Ensure instance dir exists

--- exercises/personalized_number/generator.py
+++ exercises/personalized_number/generator.py
@@ -4,5 +4,7 @@ import random
 
 instance_dir = sys.argv[1]
 number = random.randint(1, 50)
+if not os.path.exists(instance_dir):
+    os.makedirs(instance_dir)
 with open(os.path.join(instance_dir, "number"), "w") as f:
     f.write(str(number))
