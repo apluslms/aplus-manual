6f7a2bdddb712df588e898c1c4e8fba1b0c8125c
Markku Riekkinen <markku.riekkinen@aalto.fi>
2019-01-07 17:04:56 +0200

    Update the Hello Python example exercise.

--- exercises/hello_python/run.sh
+++ exercises/hello_python/run.sh
@@ -2,15 +2,21 @@
 
 # The uploaded user files are always in /submission/user
 # and named identically to config.yaml regardless of the uploaded file names.
-cd /submission/user
+# The directory /submission/user is also the default working directory
+# in the container.
 
 # The mount directory from config.yaml is in /exercise.
 # Append the required support files to test user solution.
-cp /exercise/*.py .
 
-# "capture" etc description in https://github.com/A-plus-LMS/grading-base
+# Add the working directory to the PYTHONPATH so that the grader
+# can import the student's submission. The grader program is started
+# under the path /exercise since there is no need to copy it to
+# the working directory.
+export PYTHONPATH=/submission/user
 
-capture python3 tests.py
+# "capture" etc description in https://github.com/apluslms/grading-base
+
+capture python3 /exercise/tests.py
 
 err-to-out
 grade
