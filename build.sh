fa5bb3d39892c90387931534d28bd6594adeb652
Markku Riekkinen <markku.riekkinen@aalto.fi>
2021-03-10 18:34:52 +0200

    Fix the course key in build.sh
    The old value did not match the real course key used in
    the grader.cs installation.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ git submodule init
 git submodule update
 
 # the course key in the production server grader.cs
-export COURSE_KEY='aplus-manual'
+export COURSE_KEY='aplus_manual_master'
 
 # touchrst ensures that all changes in the RST or config YAML files
 # are always taken into account in the compilation (make html).
