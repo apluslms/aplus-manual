1b0dd002551dd5d0e09cadfe10d1eb560c4c8329
Markku Riekkinen <markku.riekkinen@aalto.fi>
2022-01-04 10:53:12 +0200

    Remove git commands and COURSE_KEY from build.sh
    With the new Git Manager service, these should be removed from build.sh.

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-
-# the course key in the production server grader.cs
-export COURSE_KEY='aplus_manual_master'
-
 # touchrst ensures that all changes in the RST or config YAML files
 # are always taken into account in the compilation (make html).
 make touchrst html
