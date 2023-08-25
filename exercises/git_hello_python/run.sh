17c8032f775f14a1bda9de16b61eb7772a90bacb
Jimmy Ihalainen <jimmy.ihalainen@gmail.com>
2023-08-25 16:09:30 +0300

    Clarify git_hello_python exercise comments

--- exercises/git_hello_python/run.sh
+++ exercises/git_hello_python/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# The input git repository is written in
+# The URL for the input git repository is written in
 # /submission/user/gitsource
+
+# The repository is cloned to /submission-repo
 
 # The mount directory from config.yaml is in /exercise.
 # Append the required support files to test user solution.
@@ -12,6 +14,7 @@ if sudo-capture pre git-clone-submission /exercise/git_rsa functions.py
 then
 
   # The functions.py picked above is now in /submission/user directory.
+  # The file was moved, so it does not exist in /submission-repo anymore.
   # The rest is grading as usual.
 
   export PYTHONPATH=/submission/user
