4b958c6b5ec09b0f90a4ae6676e7450c70dfc07c
Jimmy Ihalainen <jimmy.ihalainen@gmail.com>
2023-09-01 14:10:45 +0300

    Update and fix docker-up.sh for Docker Compose v2
    Update the script so that it works better with Docker Compose v2.
    Issues with Q and S commands not being captured on Windows WSL2
    and macOS have also been fixed.
    
    Fixes #65

--- docker-up.sh
+++ docker-up.sh
@@ -22,8 +22,8 @@ else
     fi
 fi
 
-if [ -z $(which docker-compose) ]; then
-    echo "ERROR: Unable to find docker-compose. Are you sure it is installed and on your path?" >&2
+if [[ $(docker compose version) != *"version"* ]]; then
+    echo "ERROR: Unable to find docker compose plugin. Are you sure it is installed?" >&2
     exit 1
 fi
 
@@ -41,8 +41,9 @@ pid=
 keep=
 onexit() {
     trap - INT
-    # send SIGINT (^C) to childs of the docker-compose and to the process itself
-    [ "$pid" ] && { pkill -2 -P $pid; kill -2 $pid; } || true
+    # Send SIGHUP to the childs of docker compose to silence their output (detach them from controlling tty)
+    # and then stop containers.
+    [ "$pid" ] && { pkill -SIGHUP -P $pid; docker compose stop; } || true
     wait
     if [ "$keep" = "" ]; then
         clean
@@ -50,14 +51,14 @@ onexit() {
         echo "Data was not removed. You can remove it with: $0 --clean"
     fi
     rm -rf /tmp/aplus || true
-    while read -rs -t 0; do read -rs -t 0.1; done # flush input
+    while read -rs -t 0; do read -rs -t 0.1; done # Flush input
     stty sane
     exit 0
 }
 
 clean() {
     echo " !! Removing persistent data !! "
-    docker-compose down --volumes --remove-orphans
+    docker compose down --volumes --remove-orphans
     if [ "$DATA_PATH" -a -e "$DATA_PATH" ]; then
         echo "Removing $DATA_PATH"
         rm -rf "$DATA_PATH" || true
@@ -65,7 +66,7 @@ clean() {
 }
 
 update() {
-    docker-compose pull
+    docker compose pull
     res=$?
     [ $res -eq 0 ] && touch docker-compose.yml
     return $res
@@ -89,10 +90,10 @@ while [ "$1" ]; do
     shift
 done
 
-docker-compose --version
+docker compose version
 
 if [ $(($(date +%s) - $(date -r docker-compose.yml +%s))) -gt 604800 ]; then
-    # pull updates weekly
+    # Pull updates weekly
     echo "Checking for updates to the service images..."
     update
     echo
@@ -100,27 +101,27 @@ fi
 
 mkdir -p /tmp/aplus
 trap onexit INT
-docker-compose up & pid=$!
+docker compose up & pid=$!
 
-help_n=4 # show first info after 24 seconds
+help_n=4 # Show first info after 24 seconds
 while kill -0 $pid 2>/dev/null; do
-    while read -rs -t 0; do read -rs -t 0.1; done # flush input
-    read -rsn1 -t 6 i # read a byte
+    while read -rs -t 0; do read -rs -t 0.1; done # Flush input
+    read -rsn1 -t 6 i # Read a byte
     # (1 or 142) -> timeout (6s). Show help every 50 times (every 5 minutes)
     [[ $? != 0 ]] && { ((--help_n > 0)) && continue || help_n=50; }
     case "$i" in
         q|Q) break ;;
         s|S) keep="x" ; break ;;
-        $'\e') # escape (ESC or ANSI code)
-            read -rsn1 -t 0.01 i # try to read a second byte
-            [ $? -eq 142 ] && { keep="x"; break; } # timeout -> no second byte -> plain ESC
+        $'\e') # Escape (ESC or ANSI code)
+            read -rsn1 -t 0.01 i # Try to read a second byte
+            [ $? -eq 142 ] && { keep="x"; break; } # Timeout -> no second byte -> plain ESC
             ;;
     esac
 
-    # print status and help
+    # Print status and help
     echo
     echo "  List of alive containers:"
-    { docker container ls --filter "name=^${COMPOSE_PROJECT_NAME}_"  --format "{{.ID}}" | xargs docker container inspect --format '	{{.Name}}	{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}	{{range $p, $conf := .NetworkSettings.Ports}}{{$p}} {{end}}'; } 2>/dev/null || true
+    { docker container ls --filter "name=^${COMPOSE_PROJECT_NAME}-"  --format "{{.ID}}" | xargs docker container inspect --format '	{{.Name}}	{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}	{{range $p, $conf := .NetworkSettings.Ports}}{{$p}} {{end}}'; } 2>/dev/null || true
     echo
     echo "  Press Q or ^C to stop all and to remove data"
     echo "  Press S or ESC to stop all and to keep data"
